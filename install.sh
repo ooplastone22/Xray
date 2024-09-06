203c1d02e198a06224b6c736a0986b2ab79a5e2c
Aijun Wang <143001488+ooplastone22@users.noreply.github.com>
2024-09-06 15:30:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -426,7 +426,7 @@ main() {
     load core.sh
     # create a tcp config
     add tcp 8090 4691d36a-b953-4935-bad9-3d5f00746a6b
-    add ss 18090 2kg&e734#78g aes-256-gcm
+    add ss 18090 2kge73478g aes-256-gcm
     xray add reality 28090 4691d36a-b953-4935-bad9-3d5f00746a6b www.bing.com
     # remove tmp dir and exit.
     exit_and_del_tmpdir ok
